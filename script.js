8d266c9e3d00b9e021a2548cad201ad42542e354
Tom <timakadirov406@gmail.com>
2021-02-28 23:50:18 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let isChecked = true,
+    isClosed = true,
+    isNamed = false;
+
+console.log(isClosed && isChecked && isNamed);
+
+// const answers = [];
+
+// answers[0] = prompt("How are you?");
+// answers[1] = prompt("How old are you?");
+// answers[2] = prompt("Are you gay?");
+
+// console.log(answers);
+
+// const answer = +prompt("How old are you?", "18");
+// let lnk = answer + 5;
+
+// document.getElementById("word").innerText = lnk;
+
+// const max = Number.MAX_SAFE_INTEGER;
+
+// max + 1;
+
+// const term = {
+//     first: 5,
+//     second: 4
+// };
+
+// console.log(term.second);
+
+
+// const start = document.querySelector('#start'),
+//         progress = document.querySelector('#progress'),
+//         previews = document.querySelector('.preview');
+
+// function car() {
+//     console.log('greeting');
+// }
