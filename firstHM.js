1be06131a64e95cbc7694fff8e3bcc19a84d766f
Tom <timakadirov406@gmail.com>
2021-03-02 22:17:37 +0500

    new code

--- firstHM.js
+++ firstHM.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -13,30 +13,12 @@ const personalMovieDB = {
 const a = prompt('Один из последних просмотренных фильмов?', ''),
       b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
+      d = prompt('На сколько оцените его?', ''); 
+      
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
-
-// "use strict";
-
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-// let personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-// const IMDB = +prompt('На сколько оцените его?', '');
-
-// console.log(`${lastFilm}, ${IMDB}`);
-
 
 // /* Задание на урок:
 // 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
